9c0e535637adb1f0aff615e3f33affe2715c0ede
Adrian Helmchen <adrian.helmchen@outlook.de>
2024-01-26 14:35:39 +0100

    feat(Near): add getting a workflow by using a date

---
+++ NearTest/Model/DummyClassBefore.cs
@@ -0,0 +1,9 @@
+﻿using Misa;
+
+namespace NearTest.Model
+{
+    [WorkflowValidity("01.01.2018")]
+    public class DummyClassBefore : IDummyData
+    {
+    }
+}
---
+++ NearTest/Model/DummyClassBetween.cs
@@ -0,0 +1,9 @@
+﻿using Misa;
+
+namespace NearTest.Model
+{
+    [WorkflowValidity("01.05.2023")]
+    public class DummyClassBetween : IDummyData
+    {
+    }
+}
